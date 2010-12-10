fc57afbcf5a3e44e03ef0e7b28d386c1e82e1b94
Johannes Lochmann <johannes.lochmann@gmail.com>
2010-12-10 09:53:13 +0100

    added signal/slots for data changed and entity object status flag

--- src/lib/orm/Entity.cpp
+++ src/lib/orm/Entity.cpp
@@ -1,20 +1,35 @@
 #include <Entity.h>
 
-Entity::Entity(QObject* p)
-: QObject(p)
-{
+Entity::Entity(QObject* p) :
+	QObject(p) {
+	connect(this, SIGNAL(dataChanged()), this, SLOT(onDataChanged()));
+	setStatus(Entity::New);
 }
 
-Entity::Entity(const Entity& other)
-{
+Entity::Entity(const Entity& other) {
 }
 
 Entity::~Entity() {
 }
 
-void Entity::save() {}
-void Entity::refresh() {}
-void Entity::remove() {}
-void Entity::createOrUpdate() {}
-void Entity::create() {}
-void Entity::update() {} 
+void Entity::save() {
+}
+
+void Entity::refresh() {
+}
+
+void Entity::remove() {
+}
+
+void Entity::createOrUpdate() {
+}
+
+void Entity::create() {
+}
+
+void Entity::update() {
+}
+
+void Entity::onDataChanged() {
+	
+}
--- src/lib/orm/Entity.h
+++ src/lib/orm/Entity.h
@@ -4,22 +4,34 @@
 #include <QtCore/QMetaObject>
 #include <QtCore/QObject>
 
-class Entity: public QObject
-{
+class Entity : public QObject {
 	Q_OBJECT
- public:
-  Entity(QObject* p = 0);
-  virtual ~Entity();
+	Q_PROPERTY(Entity::States status READ status WRITE setStatus)
+public:
+	enum States {
+		New = 0,
+		Dirty,
+		Clean
+	};
+	Entity(QObject* p = 0);
+	virtual ~Entity();
 
-  virtual void save();
-  virtual void refresh();
-  virtual void remove();
- protected:
-  virtual void createOrUpdate();
-  virtual void create();
-  virtual void update();
- private:
-   Entity(const Entity& other);
+	virtual void save();
+	virtual void refresh();
+	virtual void remove();
+	Entity::States status() const { return _status; }
+signals:
+	void dataChanged();
+public slots:
+	void onDataChanged();
+protected:
+	virtual void createOrUpdate();
+	virtual void create();
+	virtual void update();
+	virtual void setStatus(const Entity::States& s) { _status = s; }
+private:
+	Entity(const Entity& other);
+	Entity::States _status;
 };
 
 #endif /*ENTITY_H_*/
